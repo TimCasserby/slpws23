e35168df5b2c8bc0da6294c97af4ed755a0212e2
TimCasserby <tim.casserby@elev.ga.ntig.se>
2023-02-02 11:49:28 +0100

    Basic functions
    Nu funkar formuläret och lägger till i databasen. Har även börjat jobba på en sida som visar upp alla objekt i databasen. Måste strukturera upp det mer och länka css nästa lektion.

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require 'sqlite3'
 require 'bcrypt'
 
 get('/') do
-    slim(:verktyg)
-  end
+    redirect("objects/")
+end
 
 get('/verktyg') do
     slim(:verktyg)
@@ -20,14 +20,25 @@ get('/elektronik') do
 end
 
 get('/objects/') do
-    slim(:objects)
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    objectlistfetch = db.execute("SELECT * FROM objects") # WHERE user_id = ?",id
+    p "Alla: #{objectlistfetch}"
+    slim(:"objects", locals:{objectlist:objectlistfetch})
 end
 
-get('/inlägg') do
+get('/objects/new') do
     slim(:inlägg)
 end
 
 post('/objects') do
-    # hej
-    # redirect
+    objectname = params[:name]
+    objectquantity = params[:quantity]
+    objectclass = params[:class]
+    objectstatus = params[:status]
+    objectauthor = params[:author]
+    db = SQLite3::Database.new('db/database.db')
+    db.execute('INSERT INTO objects (name, quantity, class, status, author) VALUES (?,?,?,?,?)',objectname, objectquantity, objectclass, objectstatus, objectauthor)
+    redirect("../objects/new")
+
 end
