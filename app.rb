5e3e69bac166b4a5b326c5646884a26590f9bd49
TimCasserby <tim.casserby@elev.ga.ntig.se>
2023-03-09 11:43:56 +0100

    dagens push
    - Gjort hela kommentarsystemet fungerande
    - Nästan klar med edit

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'sinatra/reloader'
+require 'date'
 
 get('/') do
     redirect("objects/")
@@ -32,13 +33,23 @@ get('/objects/new') do
     slim(:"objects/new")
 end
 
+get('/objects/:id/edit') do
+    objectid = params[:id].to_i
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    objectfetch = db.execute("SELECT * FROM objects WHERE id = ?", objectid)
+    slim(:"objects/edit", locals:{objectinfo:objectfetch})
+end
+
 get('/objects/:id/') do
     objectid = params[:id].to_i
     db = SQLite3::Database.new('db/database.db')
     db.results_as_hash = true
     objectfetch = db.execute("SELECT * FROM objects WHERE id = ?", objectid)
+    commentsfetch = db.execute("SELECT * FROM comments WHERE object_id = ?", objectid)
     p objectfetch
-    slim(:"objects/show", locals:{objectinfo:objectfetch})
+    p commentsfetch
+    slim(:"objects/show", locals:{objectinfo:objectfetch,comments:commentsfetch})
 end
 
 
@@ -60,5 +71,19 @@ post('/objects/:id/delete') do
     db = SQLite3::Database.new('db/database.db')
     db.execute("DELETE FROM objects WHERE id='#{objectid}'")
     redirect("/objects/")
+
+end
+
+
+
+post('/objects/:id/newcomment') do
+    commenttext = params[:commenttext]
+    authorid = params[:authorid]
+    objectid = params[:objectid]
+    date = DateTime.now #VI FIXAR DETTA SEN
+    date = date.strftime "%d/%m/%Y %H:%M"
+    db = SQLite3::Database.new('db/database.db')
+    db.execute('INSERT INTO comments (object_id, author_id, text, date) VALUES (?,?,?,?)',objectid, authorid, commenttext, date)
+    redirect("../objects/#{objectid}/")
 
 end
